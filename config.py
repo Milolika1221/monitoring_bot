64ecccaec669137a6b86a15fdd124f6625640433
Milolika1221 <153797512+Milolika1221@users.noreply.github.com>
2025-10-17 22:05:42 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = '25368685'  
 api_hash = 'e853dc900f04d8b21fd8e28aeb847635'  
-bot_username = '@Invest_Test_DEV_Bot' 
-alert_channel = '@testBotMonitoring12'
+bot_username = '@invest_prost_prod_2025_bot' 
+alert_channel = 'https://t.me/+xLEVQ1YCHNhkMDUy'
